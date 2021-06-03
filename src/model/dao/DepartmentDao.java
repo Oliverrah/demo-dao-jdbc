12cbb84203609adde7a7b76500d856098b0f4d07
Oliverrah <oliverrahdeleonn@gmail.com>
2021-06-03 20:43:23 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,12 @@
+package model.dao;
+
+import java.util.List;
+
+public interface DepartmentDao {
+	
+	void insert(DepartmentDao obj);
+	void update(DepartmentDao obj);
+	void deleteById(Integer id);
+	DepartmentDao findById(Integer id);
+	List<DepartmentDao> findAll();
+}
