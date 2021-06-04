05301c380de8d6bb634f9f8335252422cc6bf879
Oliverrah <oliverrahdeleonn@gmail.com>
2021-06-04 00:16:13 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,6 +11,9 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		
+		System.out.println("=== TESTE 1: seller findById ====");
+		
 		SellerDao sd = DaoFactory.createSellerDao();
 		
 		System.out.println(sd.findById(7));
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -38,9 +38,7 @@ public class SellerDaoJDBC implements SellerDao {
 		
 	}
 
-	/**
-	 *
-	 */
+
 	@Override
 	public Seller findById(Integer id) {
 		PreparedStatement ps = null;
@@ -59,17 +57,8 @@ public class SellerDaoJDBC implements SellerDao {
 			
 			if(rs.next()) {
 				
-				Department dep = new Department();
-				dep.setId(rs.getInt("DepartmentId"));
-				dep.setName(rs.getString("DepName"));
-				
-				Seller seller = new Seller();
-				seller.setId(rs.getInt("Id"));
-				seller.setName(rs.getString("Name"));
-				seller.setEmail(rs.getString("Email"));
-				seller.setBirthDate(rs.getDate("BirthDate"));
-				seller.setBaseSalary(rs.getDouble("BaseSalary"));
-				seller.setDepartment(dep);
+				Department dep = instantiateDepartment(rs); 			
+				Seller seller = instantiateSeller(rs, dep);
 				
 				return seller;
 			}
@@ -84,6 +73,26 @@ public class SellerDaoJDBC implements SellerDao {
 		
 		
 		
+	}
+
+	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+		Seller seller = new Seller();
+		seller.setId(rs.getInt("Id"));
+		seller.setName(rs.getString("Name"));
+		seller.setEmail(rs.getString("Email"));
+		seller.setBirthDate(rs.getDate("BirthDate"));
+		seller.setBaseSalary(rs.getDouble("BaseSalary"));
+		seller.setDepartment(dep);
+		
+		return seller;
+	}
+
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("DepartmentId"));
+		dep.setName(rs.getString("DepName"));
+		
+		return dep;
 	}
 
 	@Override
